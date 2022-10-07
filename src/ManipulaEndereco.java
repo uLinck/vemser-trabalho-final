7424f2017a9ccd6327bb801f8f9d72f33b750206
Gustavo Linck Ferreira <gustavo.ferreira@dbccompany.com.br>
2022-10-07 20:46:05 -0300

    update:-crud-feito

--- src/ManipulaEndereco.java
+++ src/ManipulaEndereco.java
@@ -1,23 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManipulaEndereco implements Crud<Endereco>{
+
+    List<Endereco> enderecos = new ArrayList<>();
     @Override
     public boolean criar(Endereco obj) {
-        return false;
+
+        this.enderecos.add(obj);
+
+        return true;
     }
 
     @Override
     public void listar() {
 
+     for(int i = 0; i < enderecos.size(); i++ ) {
+
+         System.out.println("Endereço id=" + i + " | " + enderecos.get(i));
+
+     }
+
     }
 
     @Override
     public boolean atualizar(int idx, Endereco obj) {
-        return false;
+
+        Endereco atualizarEndereco = enderecos.get(idx);
+
+        atualizarEndereco.setCep(obj.getCep());
+        atualizarEndereco.setRua(obj.getRua());
+        atualizarEndereco.setPais(obj.getPais());
+        atualizarEndereco.setComplemento(obj.getComplemento());
+        atualizarEndereco.setEstado(obj.getEstado());
+        atualizarEndereco.setCidade(obj.getCidade());
+        atualizarEndereco.setNumero(obj.getNumero());
+
+        return true;
     }
 
     @Override
     public boolean deletar(int idx) {
-        return false;
+
+        this.enderecos.remove(idx);
+
+        return true;
     }
 }
