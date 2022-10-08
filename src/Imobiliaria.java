12f7e62dc099ee21f8bfdf273ea1cdab7054cb89
Gustavo Linck Ferreira <gustavo.ferreira@dbccompany.com.br>
2022-10-08 19:59:04 -0300

    update:-implementando-menu-imoveis

--- src/Imobiliaria.java
+++ src/Imobiliaria.java
@@ -10,22 +10,23 @@ public class Imobiliaria {
     public static final String AMARELO = "\u001B[33m";
     public static final String RESET = "\u001B[0m";
 
+    static Scanner linha = new Scanner(System.in);
+    static ManipulaCliente crudCliente = new ManipulaCliente();
+    static ManipulaEndereco crudEndereco = new ManipulaEndereco();
+    static ManipulaImovel crudImovel = new ManipulaImovel();
+    static ManipulaContrato crudContrato = new ManipulaContrato();
+
     public static void main(String[] args) {
-        Scanner linha = new Scanner(System.in);
-        ManipulaCliente crudCliente = new ManipulaCliente();
-        ManipulaEndereco crudEndereco = new ManipulaEndereco();
-        ManipulaImovel crudImovel = new ManipulaImovel();
-        ManipulaContrato crudContrato = new ManipulaContrato();
         boolean execucao = true;
         while(execucao){
             try{
-                int opcao = menuInicial(linha);
+                int opcao = menuInicial();
                 switch(opcao){
                     case 1 -> {
                         opcao = menuCliente(linha);
                         switch (opcao){
                             case 1 ->{
-                                crudCliente.criar(pegarInformaçõesCliente(linha));
+                                crudCliente.criar(pegarInformacoesCliente());
                                 System.out.println("Cliente cadastrado com Sucesso!");
                             }
                             case 2 ->{
@@ -34,7 +35,7 @@ public class Imobiliaria {
                                 opcao = linha.nextInt();
                                 linha.nextLine();
                                 crudCliente.buscarCliente(opcao);
-                                crudCliente.atualizar(opcao,pegarInformaçõesCliente(linha));
+                                crudCliente.atualizar(opcao, pegarInformacoesCliente());
                                 System.out.println("Atualizado com Sucesso!");
                             }
                             case 3 -> {
@@ -65,11 +66,52 @@ public class Imobiliaria {
                                 throw new OpcaoInvalidaException();
                             }
                         }
+                    }
 
-                    }case 2->{//MENU IMOVEL
+                    case 2 -> {//MENU IMOVEL
+                        opcao = menuImoveis(linha);
+                        switch (opcao) {
 
+                            case 1 -> {
+                                var imovel = pegarInformacoesImoveis();
+                                if (imovel != null) {
+                                    crudImovel.criar(imovel);
+                                } else {
+                                    System.out.println("Deu algo errado, tente novamente!");
+                                }
+                            }
 
-                    }case 3 ->{//MENU CONTRATOS
+                            case 2 -> {
+                                System.out.println("\nQual imóvel você deseja atualizar?");
+                                crudImovel.listar();
+                                opcao = linha.nextInt();
+                                linha.nextLine();
+                                crudImovel.atualizar(opcao, pegarInformacoesImoveis());
+                                System.out.println("Atualizado com Sucesso!");
+                            }
+                            case 3 -> {
+                                crudImovel.listar();
+                            }
+                            case 4 -> {
+                                System.out.println("\nQual imóvel você deseja Remover?");
+                                crudImovel.listar();
+                                opcao = linha.nextInt();
+                                linha.nextLine();
+                                System.err.print("\nTem certeza que deseja remover o imóvel? (Sim ou Não)\n");
+                                if(linha.nextLine().equalsIgnoreCase("sim")){
+                                    crudImovel.deletar(opcao);
+                                    System.err.println("Deletado com Sucesso!");
+                                }else {
+                                    System.err.println("Remover Cancelado!");
+
+                                }
+                            }
+                            case 5 -> {
+                                //FAZER BUSCAR IMOVEL, IREI FAZER DEPOIS(GUSTAVO)
+                            }
+                        }
+
+                    }case 3 -> {//MENU CONTRATOS
 
                     } default -> {
                         throw new OpcaoInvalidaException();
@@ -89,8 +131,8 @@ public class Imobiliaria {
         }
     }
 
-    private static int menuInicial(Scanner linha) throws OpcaoInvalidaException{
-        System.out.println(AMARELO +"Imobiliaria Shinigamis"+RESET);
+    private static int menuInicial() throws OpcaoInvalidaException{
+        System.out.println(AMARELO +"\nImobiliaria Shinigamis"+RESET);
         System.out.println("Escolha uma das opções abaixo: ");
         System.out.println(AZUL+"1- Menu Cliente"+RESET);
         System.out.println(ROXO+"2- Menu Imovel"+RESET);
@@ -116,10 +158,10 @@ public class Imobiliaria {
         return opcao;
     }
 
-    private static Cliente pegarInformaçõesCliente(Scanner linha) throws DadoInvalidoException{
+    private static Cliente pegarInformacoesCliente() throws DadoInvalidoException{
         System.out.println(AZUL+"\nInforme o nome do cliente");
         String nome = linha.nextLine();
-        System.out.println("\nInforme o cpf do cliente");
+        System.out.println("\nInforme o CPF do cliente");
         String cpf = linha.nextLine();
         System.out.println("\nInforme o email do cliente");
         String email = linha.nextLine();
@@ -138,5 +180,115 @@ public class Imobiliaria {
     }
 
 
+    private static int menuImoveis(Scanner linha) throws OpcaoInvalidaException{
+
+        System.out.println(AMARELO +"Imobiliaria Shinigamis"+RESET);
+        System.out.println("Escolha uma das opções do Menu Imoveis: ");
+        System.out.println(ROXO+"1- Cadastrar imóvel");
+        System.out.println("2- Editar informações do imóvel");
+        System.out.println("3- Listar imóveis");
+        System.out.println("4- Remover imóvel");
+        System.out.println("5- Buscar imóvel");
+        System.out.println(VERMELHO+"9- SAIR"+RESET);
+        int opcao = linha.nextInt();
+        linha.nextLine();
+        return opcao;
+    }
+
+    private static Imovel pegarInformacoesImoveis() throws DadoInvalidoException {
+        System.out.println(ROXO+"- Informe o endereço do novo imóvel -");
+        Endereco endereco = new Endereco();
+        System.out.println("Digite o CEP do novo endereço:");
+        endereco.setCep(linha.nextLine());
+        System.out.println("Digite o País do novo endereço:");
+        endereco.setPais(linha.nextLine());
+        System.out.println("Digite a Cidade do novo endereço:");
+        endereco.setCidade(linha.nextLine());
+        System.out.println("Digite o Estado do novo endereço:");
+        endereco.setEstado(linha.nextLine());
+        System.out.println("Digite a Rua do novo endereço:");
+        endereco.setRua(linha.nextLine());
+        System.out.println("Digite o Complemento do novo endereço:");
+        endereco.setComplemento(linha.nextLine());
+        System.out.println("Digite o Número do novo endereço:");
+        endereco.setNumero(linha.nextLine());
+        crudEndereco.criar(endereco);
+        System.out.println("\n- Endereço do imóvel criado com sucesso! - ");
+        System.out.println("Que tipo de imóvel você está informando?(Apartamento/Casa)");
+        String tipo = linha.nextLine().toLowerCase();
+
+
+        if (tipo.equals("apartamento") || tipo.equals("casa")) {
+            if (tipo.equals("apartamento")) {
+                return pegarInformacoesGeraisImoveis(TipoImovel.APARTAMENTO, endereco).apartamento;
+            } else {
+                return pegarInformacoesGeraisImoveis(TipoImovel.CASA, endereco).casa;
+            }
+        } else {
+            System.out.println("Dado inválido!");
+        }
+
+        return null;
+    }
+
+    private static CasaEApartamento pegarInformacoesGeraisImoveis(TipoImovel tipo, Endereco endereco) throws DadoInvalidoException {
+
+        CasaEApartamento imovel = new CasaEApartamento();
+
+        System.out.println("Quantos quartos este imóvel contêm?");
+        int quartoQntd = linha.nextInt();
+
+        System.out.println("Quantos banheiros este imóvel contêm?");
+        int banheiroQntd = linha.nextInt();
+
+        System.out.println("Valor mensal deste imóvel:");
+        double mensalValor = linha.nextDouble();
+
+        System.out.println("Valor do condomínio deste imóvel:");
+        double condominioValor = linha.nextDouble(); linha.nextLine();
+
+        if (tipo == TipoImovel.CASA) {
+
+            System.out.println("Essa casa possui area de lazer? (Sim/Não)");
+            boolean lazer = linha.nextLine().equalsIgnoreCase("sim");
+
+
+            System.out.println("Essa casa possui garagem? (Sim/Não)");
+            boolean garagem = linha.nextLine().equalsIgnoreCase("sim");
+
+            imovel.casa.setTipoImovel(TipoImovel.CASA);
+            imovel.casa.setQntdQuartos(quartoQntd);
+            imovel.casa.setQntdBanheiros(banheiroQntd);
+            imovel.casa.setValorMensal(mensalValor);
+            imovel.casa.setCondominio(condominioValor);
+            imovel.casa.setAreaDeLazer(lazer);
+            imovel.casa.setGaragem(garagem);
+            imovel.casa.setEndereco(endereco);
+
+        } else if (tipo == TipoImovel.APARTAMENTO) {
+
+            System.out.println("Neste apartamento permite animais?");
+            boolean animais = linha.nextLine().equalsIgnoreCase("sim");
+
+            System.out.println("Neste apartamento há salão de festa?");
+            boolean festa = linha.nextLine().equalsIgnoreCase("sim");
+
+            System.out.println("Quantas vagas há neste apartamento?");
+            int vagas = linha.nextInt();
+
+            imovel.apartamento.setTipoImovel(TipoImovel.APARTAMENTO);
+            imovel.apartamento.setQntdQuartos(quartoQntd);
+            imovel.apartamento.setQntdBanheiros(banheiroQntd);
+            imovel.apartamento.setValorMensal(mensalValor);
+            imovel.apartamento.setCondominio(condominioValor);
+            imovel.apartamento.setPermiteAnimais(animais);
+            imovel.apartamento.setSalaoDeFesta(festa);
+            imovel.apartamento.setNumeroDeVagas(vagas);
+            imovel.apartamento.setEndereco(endereco);
+
+        }
+
+        return imovel;
+    }
 
 }
