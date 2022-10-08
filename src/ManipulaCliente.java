3e6bcf2b5524795aa5e52b90091466b2a8d16449
Gustavo Linck Ferreira <gustavo.ferreira@dbccompany.com.br>
2022-10-08 16:05:35 -0300

    fix:-tirando-import-inutilizado

--- src/ManipulaCliente.java
+++ src/ManipulaCliente.java
@@ -2,7 +2,6 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class ManipulaCliente implements Crud<Cliente>{
     List<Cliente> clientes = new ArrayList<>();
